c8bf5dc5af3bfe9ddadaa6a2b7d2d4a5f593a76b
halanzi <homoudnan@gmail.com>
2021-02-02 18:13:18 +0300

    I am done, I think

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
 // Define the numbers and the operation here
-
+const var1 = 5;
+const var2 = 3;
+let plus = "+";
 // Write if-statements here
+if (var1 > var2) {
+    console.log("This is an addition")
+    console.log(`${var1} + ${var2} =${var1 + var2}`)
+} else console.log("This isn't an addition operation")
