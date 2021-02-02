66037dc7edf208c7d362da2e6c10cea972b78567
halanzi <homoudnan@gmail.com>
2021-02-02 20:21:14 +0300

    Task re-done

--- calculator.js
+++ calculator.js
@@ -1,9 +1,24 @@
 // Define the numbers and the operation here
 const var1 = 5;
 const var2 = 3;
-let plus = "+";
+let Operation = "/";
 // Write if-statements here
-if (var1 > var2) {
-    console.log("This is an addition")
-    console.log(`${var1} + ${var2} =${var1 + var2}`)
+if (Operation === "+") {
+    console.log("This is an addition operation")
+    console.log(`${var1} + ${var2} = ${var1 + var2}`)
 } else console.log("This isn't an addition operation")
+
+if (Operation === "-") {
+    console.log("This is a subtraction operation")
+    console.log(`${var1} - ${var2} = ${var1 - var2}`)
+} else console.log("This isn't an subtraction operation")
+
+if (Operation === "*") {
+    console.log("This is a multiplication operation")
+    console.log(`${var1} * ${var2} = ${var1 * var2}`)
+} else console.log("This isn't a multiplication operation")
+
+if (Operation === "/") {
+    console.log("This is an division operation")
+    console.log(`${var1} / ${var2} = ${var1 / var2}`)
+} else console.log("This isn't a division operation")
